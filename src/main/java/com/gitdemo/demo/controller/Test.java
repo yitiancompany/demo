db48eb73ff3594fd872823eb7bee2531ae03b695
zhengxinbin <chinazheng9990@163.com>
2021-03-04 13:41:46 +0800

    first commit

---
+++ src/main/java/com/gitdemo/demo/controller/Test.java
@@ -0,0 +1,21 @@
+package com.gitdemo.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhengxinbin
+ * @program: zhengxinbin
+ * @description: 测试
+ * @create 2021-03-04 13:21:17
+ */
+@RestController
+@RequestMapping("/")
+public class Test {
+    @RequestMapping("/test")
+    public String sample()
+    {
+        return "测试!!!";
+    }
+
+}
